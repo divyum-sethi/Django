d744c108dd328d84b372d1d1b312e3ec5b783935
Divyum Sethi <32799748+divyum-sethi@users.noreply.github.com>
2020-08-01 23:55:46 +0530

    Update settings.py

--- tasks/tasks/settings.py
+++ tasks/tasks/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '3$88ngq&92nx3x=i3p&9%fl$rr_wsb2-g_aka=_d8!$mwh1zs2'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
